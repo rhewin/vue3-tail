c07230855156dba6f7c529683615645312664a07
Rheza Sindhuwinata <rsindhuwinata@prixa.ai>
2022-03-12 12:48:51 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
